ecb2bfe6ea769d58e3ffc53e03bddd91f8054795
david <davvid.davids@gmail.com>
2017-12-08 17:19:13 +0200

    Configure MongoDB node.

---
+++ DB/conf2.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+ufw allow proto tcp from 46.101.201.237 to any port 27019
+ufw allow proto tcp from 46.101.201.237 to any port 27018
+ufw allow proto tcp from 46.101.201.237 to any port 27017
+ufw allow proto tcp from 46.101.152.227 to any port 27019
+ufw allow proto tcp from 46.101.152.227 to any port 27018
+ufw allow proto tcp from 46.101.152.227 to any port 27017
+
+
